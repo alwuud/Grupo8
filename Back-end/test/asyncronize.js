55fae1706edc927af9e74af22082caca2d9bd0a8
Jonnathan Castillo <jonnantonicas@hotmail.com>
2019-08-22 00:32:16 -0600

    Cambios test

--- Back-end/test/asyncronize.js
+++ Back-end/test/asyncronize.js
@@ -1,2 +1,30 @@
 //Para pruebas asincronicas
 //Empezando pruebas unitarias
+before(done => {
+    console.log('\n\n-----------------------\n--\n-- START TEST\n--\n-------------------------');
+    done();
+});
+after(done => {
+    console.log('\n\n-----------------------\n--\n-- END TEST\n--\n-------------------------');
+    done();
+});
+/* asyn test */
+describe('#Asynchronous user crud test', () => {
+    it('get "users" record', done => {
+        chai.request(server)
+            .get('/users')
+            .end(function (err, res) {
+                if(err) done(err);
+                done();
+                console.log('status code: %s, users: %s',res.statusCode, res.body.length)
+            });
+    }).timeout(0);
+    it('get "user by id" record', done => {
+        chai.request(server)
+            .get('/users/1')
+            .end(function (err, res) {
+                if(err) done(err);
+                done();
+                console.log('status code: %s, user: %s',res.statusCode, util.inspect(res.body, false, null))
+            });
+    }).timeout(0);
--- Back-end/test/syncronize.js
+++ Back-end/test/syncronize.js
@@ -1,2 +1,37 @@
 //Para pruebas sincronicas
 //Empezando pruebas unitarias
+before(done => {
+    console.log('\n\n-----------------------\n--\n-- START TEST\n--\n-------------------------');
+    done();
+});
+after(done => {
+    console.log('\n\n-----------------------\n--\n-- END TEST\n--\n-------------------------');
+    done();
+});
+/* asyn test */
+describe('#Synchronous user crud test', () => {
+    describe('## get users', () => {
+        it('get "users" record', done => {
+            chai.request(server)
+                .get('/users')
+                .end(function (err, res) {
+                    if (err) done(err);
+                    done();
+                    console.log('status code: %s, users: %s', res.statusCode, res.body.length)
+                });
+        }).timeout(0);
+    })
+    describe('## Save Users', () => {
+        it('save "user" record', done => {
+            chai.request(server)
+                .post('/users')
+                .send(user)
+                .then(res => {
+                    done();
+                    console.log('status code: %s, user saved with id: %s', res.statusCode, res.body.insertId)
+                })
+                .catch(err => {
+                    done(err);
+                });
+        }).timeout(0);
+    })
